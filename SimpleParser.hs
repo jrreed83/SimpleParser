ee937d6ff28538d15e2714135123a596146ebb8f
jrreed83 <joeyrreed@gmail.com>
2017-10-21 20:41:07 -0700

    jrr

--- SimpleParser.hs
+++ SimpleParser.hs
@@ -1,33 +1,33 @@
 module SimpleParser where
 
-    data Result = Success {match::String, rest::String}
-                | Failure {msg::String}
-                deriving (Show, Eq)
+    data Result a = Success {match :: a, rest :: String}
+                  | Failure {msg :: String}
+                  deriving (Show, Eq)
 
-    data Parser = Parser {run :: String -> Result}
+    data Parser a = Parser {run :: String -> Result a}
 
-    pChar :: Char -> Parser
+    pChar :: Char -> Parser Char
     pChar c =
         Parser $
              \s -> 
                 if (head s) == c then 
-                    Success [c] (tail s)  
+                    Success c (tail s)  
                 else 
                     Failure ("Expected " ++ [c] ++ " but got " ++ [head s])          
              
-    andThen :: Parser -> Parser -> Parser
+    andThen :: Parser a -> Parser b -> Parser (a,b)
     andThen parser1 parser2 = 
         Parser $
              \s -> case run parser1 s of 
                         Failure x      -> Failure x 
                         Success m1 r1  -> case run parser2 r1 of
                                                Failure x      -> Failure x 
-                                               Success m2 r2  -> Success (m1++m2) r2   
+                                               Success m2 r2  -> Success (m1,m2) r2   
 
-    (.>>.) :: Parser -> Parser -> Parser 
+    (.>>.) :: Parser a -> Parser b -> Parser (a,b) 
     (.>>.) = andThen
 
-    alt :: Parser -> Parser -> Parser 
+    alt :: Parser a -> Parser a -> Parser a 
     alt parser1 parser2 = 
         Parser $
              \s -> case run parser1 s of
@@ -36,9 +36,9 @@ module SimpleParser where
                                           Failure _   -> Failure "Couldn't match anything"
                                           Success m r -> Success m r 
 
-    (<|>) :: Parser -> Parser -> Parser 
+    (<|>) :: Parser a -> Parser a -> Parser a 
     (<|>) = alt
 
-    pDigit :: Parser
+    pDigit :: Parser Char
     pDigit =  (pChar '0') <|> (pChar '1') <|> (pChar '2') <|> (pChar '3') <|> (pChar '4') 
           <|> (pChar '5') <|> (pChar '6') <|> (pChar '7') <|> (pChar '8') <|> (pChar '9') 
