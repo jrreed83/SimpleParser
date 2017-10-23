2cca26ad993609806f7e6c2bd30e8803ed2fa788
jrreed83 <joeyrreed@gmail.com>
2017-10-23 06:10:47 -0700

    jrr

--- SimpleParser.hs
+++ SimpleParser.hs
@@ -9,7 +9,7 @@ module SimpleParser where
     instance Functor Parser where
         fmap f pa = Parser $ 
                         \s -> case run pa s of 
-                                   Failure _   -> Failure "Error" 
+                                   Failure m   -> Failure m 
                                    Success x c -> Success (f x) c        
              
     pChar :: Char -> Parser Char
@@ -41,7 +41,7 @@ module SimpleParser where
              \s -> case run parser1 s of 
                         Failure x      -> Failure x 
                         Success m1 c1  -> case run parser2 (drop c1 s) of
-                                               Failure x      -> Failure x 
+                                               Failure m      -> Failure m 
                                                Success m2 c2  -> Success (m1,m2) (c1+c2)   
 
     (.>>.) :: Parser a -> Parser b -> Parser (a,b) 
@@ -53,7 +53,7 @@ module SimpleParser where
              \s -> case run parser1 s of
                         Success m c -> Success m c
                         Failure _   -> case run parser2 s of 
-                                            Failure _   -> Failure "Couldn't match anything"
+                                            Failure m   -> Failure "Couldn't match anything"
                                             Success m c -> Success m c 
 
     (<|>) :: Parser a -> Parser a -> Parser a 
@@ -63,7 +63,7 @@ module SimpleParser where
     slice p = 
         Parser $
              \s -> case run p s of
-                        Failure _   -> Failure "Error"
+                        Failure m   -> Failure m
                         Success _ c -> Success (take c s) c
     
     map2 :: ((a,b) -> c) -> Parser a -> Parser b -> Parser c
@@ -71,6 +71,17 @@ module SimpleParser where
         Parser $
              \s -> case run pa s of
                         Failure _      -> Failure "Error"
-                        Success xa ta  -> case run pb s of 
+                        Success xa ta  -> case run pb (drop ta s) of 
                                                Failure _     -> Failure "Error"
-                                               Success xb tb -> Success (f (xa,xb)) (max ta tb)  
+                                               Success xb tb -> Success (f (xa,xb)) (ta + tb)  
+
+
+    map2' :: ((a,b) -> c) -> Parser a -> Parser b -> Parser c
+    map2' f pa pb = fmap f (pa .>>. pb)
+
+    many :: Parser a -> Parser [a]
+    many pa = 
+        Parser (fn [] 0)
+        where fn lst cnt s = case run pa s of
+                Failure _    -> Success lst cnt
+                Success x t  -> fn (lst ++ [x]) (cnt + t) (drop t s)
