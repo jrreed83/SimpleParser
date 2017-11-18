805404e9f0941ee6b57567729e4ab3c43bff3db2
jrreed83 <joeyrreed@gmail.com>
2017-11-17 19:53:25 -0800

    jrr

--- examples/ARM.hs
+++ examples/ARM.hs
@@ -28,35 +28,42 @@ where
     comments :: Parser String 
     comments = semicolon >> anyString
 
-    immediate :: Parser Exp
+    immediate :: Parser Immed
     immediate = do { _ <- string "#"
                    ; d <- integer
-                   ; return $ Num (asWord32 d)}
+                   ; return $ Immed (asWord32 d)}
 
-    register :: Parser Exp 
+    register :: Parser Reg 
     register = do { _ <- char 'r' 
                   ; i <- integer 
                   ; return $ Reg (asWord8 i) }
 
+    data Reg = Reg W.Word32 deriving Show 
+    
+    data Immed = Immed W.Word32 deriving Show 
+
     data Op = ADD | SUB | MOVE | STORE | LOAD | DEREF deriving (Show)
 
-    data Exp = Exp3 Op Exp Exp Exp
-             | Exp2 Op Exp Exp 
-             | Exp1 Op Exp
-             | Reg W.Word8
-             | Num W.Word32
+    data Exp = Exp3 Op Reg Reg Reg
+             | Exp2 Op Reg Reg 
              deriving (Show)    
 
---    data DataProcessing = DataProcessing  { cond :: ! W.Word8 
---                                          , Exp1  :: ! W.Word8
---                                          , rn   :: ! W.Word8
---                                          , rs   :: ! W.Word8 
---                                          , Exp2  :: ! W.Word8}
 
---    u8 :: Parser W.Word8 
---    u8 
+    encodeOp :: Op -> W.Word32 
+    encodeOp ADD   = 1 
+    encodeOp SUB   = 2 
+    encodeOp MOVE  = 3
+    encodeOp STORE = 4
+    encodeOp LOAD  = 5 
+    encodeOp DEREF = 6
 
---    u16 :: Parser W.Word16 
+    decodeOp :: W.Word32 -> Op 
+    decodeOp 1 = ADD 
+    decodeOp 2 = SUB 
+    decodeOp 3 = MOVE 
+    decodeOp 4 = STORE 
+    decodeOp 5 = LOAD 
+    decodeOp 6 = DEREF 
 
 
     asWord16 :: (Integral a) => a -> W.Word16 
@@ -98,57 +105,57 @@ where
              ; rm  <- register 
              ; return $ Exp3 SUB rd rn rm } 
 
-    move :: Parser Exp
-    move = do { _   <- string "MOV" 
-              ; _   <- spaces 
-              ; rd  <- register 
-              ; _   <- comma 
-              ; _   <- spaces 
-              ; rn  <- register <|> immediate 
-              ; return $ Exp2 MOVE rd rn }            
+--    move :: Parser Exp
+--    move = do { _   <- string "MOV" 
+--              ; _   <- spaces 
+--              ; rd  <- register 
+--              ; _   <- comma 
+--              ; _   <- spaces 
+--              ; rn  <- register <|> immediate 
+--              ; return $ Exp2 MOVE rd rn }            
     
-    derefOffset :: Parser Exp 
-    derefOffset = do { _      <- lbracket
-                     ; rn     <- register
-                     ; _      <- comma 
-                     ; _      <- spaces
-                     ; offset <- immediate 
-                     ; _      <- rbracket
-                     ; return $ Exp2 DEREF rn offset } 
+--    derefOffset :: Parser Exp 
+--    derefOffset = do { _      <- lbracket
+--                     ; rn     <- register
+--                     ; _      <- comma 
+--                     ; _      <- spaces
+--                     ; offset <- immediate 
+--                     ; _      <- rbracket
+--                     ; return $ Exp2 DEREF rn offset } 
 
-    derefNoOffset :: Parser Exp 
-    derefNoOffset = do { _      <- lbracket
-                       ; rn     <- register  
-                       ; _      <- rbracket
-                       ; return $ Exp2 DEREF rn (Num 0) } 
+--    derefNoOffset :: Parser Exp 
+--    derefNoOffset = do { _      <- lbracket
+--                       ; rn     <- register  
+--                       ; _      <- rbracket
+--                       ; return $ Exp2 DEREF rn (Num 0) } 
 
-    deref :: Parser Exp 
-    deref = derefOffset <|> derefNoOffset  
+--    deref :: Parser Exp 
+--    deref = derefOffset <|> derefNoOffset  
     
-    load :: Parser Exp
-    load = do { _  <- string "LDR"
-              ; _  <- spaces
-              ; rd <- register
-              ; _  <- comma 
-              ; _  <- spaces 
-              ; d  <- deref 
-              ; return $ Exp2 LOAD rd d }
+--    load :: Parser Exp
+--    load = do { _  <- string "LDR"
+--              ; _  <- spaces
+--              ; rd <- register
+--              ; _  <- comma 
+--              ; _  <- spaces 
+--              ; d  <- deref 
+--              ; return $ Exp2 LOAD rd d }
 
-    store :: Parser Exp
-    store = do { _  <- string "STR"
-             ; _  <- spaces
-             ; rn <- (label "expect register" register)
-             ; _  <- comma 
-             ; _  <- spaces 
-             ; d  <- deref 
-             ; return $ Exp2 STORE rn d }
+--    store :: Parser Exp
+--    store = do { _  <- string "STR"
+--             ; _  <- spaces
+--             ; rn <- (label "expect register" register)
+--             ; _  <- comma 
+--             ; _  <- spaces 
+--             ; d  <- deref 
+--             ; return $ Exp2 STORE rn d }
 
-    dump :: Parser Exp
-    dump = (string ":dump") >>. (return $ Reg 0)
+    --dump :: Parser Exp
+    --dump = (string ":dump") >>. (return $ Reg 0)
 
     anyExpression :: Parser Exp 
     anyExpression = do { _   <- spaces
-                       ; exp <- move <|> add <|> sub <|> store <|> load <|> register <|> immediate
+                       ; exp <- add <|> sub 
                        ; _   <- eol 
                        ; return exp }
     
@@ -191,7 +198,8 @@ where
            ; rn <- readIORef (r !! n')
            ; _  <- writeIORef (r !! d') (rm + rn)
            ; rd <- readIORef (r !! d')
-           ; return rd }
+           ; return rd 
+        }
 
     foo :: IO () 
     foo = do { cpu <- initCPU 
